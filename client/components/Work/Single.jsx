ff6a64e94f26db98667e16f1327fcb089538d09c
iamkevingreen <kevin@redantler.com>
2016-02-25 16:28:49 -0500

    pull in a real single post from prismic

--- client/components/Work/Single.jsx
+++ client/components/Work/Single.jsx
@@ -6,21 +6,40 @@ import emitter from '../../emitters/default'
 import Helmet from 'react-helmet'
 
 let WorkSingle = React.createClass({
+  getInitialState() {
+    return {
+      doc: [],
+      meta: []
+    }
+  },
   componentWillMount() {
     this.getData()
   },
   getData() {
     console.log('hey');
     let api = new API();
-    let slug = this.props.slug;
+    let slug = this.props.params;
 
-    console.log(this.props);
+    console.log('slug', slug);
+    api.query('my.projects.uid', slug).then((response) => {
+      let record = response.results[0];
+
+      this.setState({
+        doc: record,
+        meta: {
+          title: record.get('projects.title').asText(),
+          image: record.getImage('projects.hero_image').url
+        }
+      });
+
+    })
   },
   render() {
     return (
       <div className="page--background">
         <div className="site--wrapper">
-          Test
+          <h2>{this.state.meta.title}</h2>
+          <img src={this.state.meta.image} />
         </div>
       </div>
     )
