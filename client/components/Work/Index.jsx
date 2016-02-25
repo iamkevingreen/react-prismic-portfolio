3fa5a483517f2e8d3b6b26dd82b21b0a0a77a915
iamkevingreen <kevin@redantler.com>
2016-02-25 12:11:55 -0500

    heroku was ignoring the uppercase to lowercase file change

---
+++ client/components/Work/Index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class WorkIndex extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Work information</h2>
+      </div>
+    )
+  }
+}
+
+WorkIndex.defaultProps = {
+  works: []
+};
+
+export default WorkIndex;
